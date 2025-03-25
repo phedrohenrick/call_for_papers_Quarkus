8a4b2e59ffc71e3f9285f2b12417b0e9867211d7
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-25 15:17:15 -0300

    feat: adiciona alguns testes simples de criação de palestras

---
+++ call-for-papers/src/test/java/or/br/callForPaper/DesafioCallForPapersAppTests.java
@@ -0,0 +1,57 @@
+package or.br.callForPaper;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.br.callForPaper.dto.PaperDetailsDTO;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+@QuarkusTest
+class DesafioCallForPapersAppTests {
+
+
+    @Test
+    void testCreatePaperSuccess() {
+        PaperDetailsDTO paper = new PaperDetailsDTO( "titulo", "resumo", "nomeDoAutor", "email");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(paper)
+                .when().post("/api/paper/")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("titulo", equalTo(paper.getTitulo()))
+                .body("resumo", equalTo(paper.getResumo()))
+                .body("nomeDoAutor", equalTo(paper.getNomeDoAutor()))
+                .body("email", equalTo(paper.getEmail()));
+    }
+
+    @Test
+    public void testCreatePaperFailure_MissingFields() {
+
+        PaperDetailsDTO invalidPaper = new PaperDetailsDTO("", "", "", "");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(invalidPaper)
+                .when().post("/api/paper/")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("parameterViolations.size()", greaterThan(0))
+                .body("parameterViolations.find { it.path == 'createPaper.paperDetailsDTO.titulo' }.message", equalTo("o conteúdo é obrigatório"))
+                .body("parameterViolations.find { it.path == 'createPaper.paperDetailsDTO.resumo' }.message", equalTo("o conteúdo é obrigatório"))
+                .body("parameterViolations.find { it.path == 'createPaper.paperDetailsDTO.nomeDoAutor' }.message", equalTo("o conteúdo é obrigatório"))
+                .body("parameterViolations.find { it.path == 'createPaper.paperDetailsDTO.email' }.message", equalTo("o conteúdo é obrigatório"));
+    }
+
+
+}
+
+
+
+
+
