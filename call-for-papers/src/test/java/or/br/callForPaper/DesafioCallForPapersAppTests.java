c4c7cc41ca13690a033a9044c7f968501a2f3363
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-26 17:04:33 -0300

    test: adiciona testes para update, delete e listagem de papers
    - Implementa teste de sucesso para atualização de paper via PUT
    - Adiciona teste para exclusão de paper via DELETE
    - Cria teste para listagem de papers via GET,
    - Valida os status codes esperados e a consistência dos dados retornados

--- call-for-papers/src/test/java/or/br/callForPaper/DesafioCallForPapersAppTests.java
+++ call-for-papers/src/test/java/or/br/callForPaper/DesafioCallForPapersAppTests.java
@@ -52,7 +52,116 @@ class DesafioCallForPapersAppTests {
                 .body("parameterViolations.find { it.path == 'createPaper.paperDetailsDTO.email' }.message", equalTo("o conteúdo é obrigatório"));
     }
 
+    @BeforeEach
+    void setup() {
+        // Criando um paper antes do teste
+        PaperDetailsDTO paper = new PaperDetailsDTO(null, "Título Original", "Resumo Original", "Autor Original", "autor@email.com");
+        PaperDetailsDTO paper2 = new PaperDetailsDTO(null, "Título Original2", "Resumo Original2", "Autor Original2", "autor2@email.com");
 
+        paperId = given()
+                .contentType(ContentType.JSON)
+                .body(paper)
+                .when().post("/api/paper/")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().jsonPath().getLong("id");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(paper2)
+                .when().post("/api/paper/")
+                .then()
+                .log().body()
+                .statusCode(201);
+
+
+        log.info(String.valueOf(paperId));
+
+    }
+
+
+    @Test
+    public void testUpdatePaperSuccess() {
+        PaperDetailsDTO updatedPaper = new PaperDetailsDTO(paperId, "Novo Título", "Novo Resumo", "Autor Original", "autor@email.com");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedPaper)
+                .when()
+                .put("/api/paper/update/" + paperId)
+                .then()
+                .statusCode(200)
+                .body("titulo", equalTo("Novo Título"))
+                .body("resumo", equalTo("Novo Resumo"))
+                .body("nomeDoAutor", equalTo("Autor Original"))
+                .body("email", equalTo("autor@email.com"));
+
+
+    }
+
+    @Test
+    public void testUpdatePaperFailure_UnrecognizedPaper() {
+        Long invalidId = 9999L;
+
+        PaperDetailsDTO updatedPaper = new PaperDetailsDTO(paperId, "Novo Título", "Novo Resumo", "Autor Original", "autor@email.com");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedPaper)
+                .when()
+                .put("/api/paper/update/" + invalidId)
+                .then()
+                .statusCode(404)
+                .body(equalTo("A submissão que deseja editar não existe"));
+    }
+
+    @Test
+    public void testDeletePaperSuccess() {
+        // Criar um novo paper para garantir que o ID existe
+        PaperDetailsDTO paper = new PaperDetailsDTO(null, "Título para Deletar", "Resumo", "Autor", "autor@email.com");
+
+        Long paperIdToDelete = given()
+                .contentType(ContentType.JSON)
+                .body(paper)
+                .when()
+                .post("/api/paper")
+                .then()
+                .statusCode(201)
+                .extract().jsonPath().getLong("id");
+
+
+        given()
+                .when()
+                .delete("/api/paper/" + paperIdToDelete)
+                .then()
+                .statusCode(204); // Espera um retorno 204 No Content
+
+    }
+
+    @Test
+    public void testDeletePaperFailure_UnrecognizedId() {
+        Long paperUnrecognizedId = 999L;
+
+
+        given()
+                .when()
+                .delete("/api/paper/" + paperUnrecognizedId)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testlistPapers(){
+
+        given()
+                .when()
+                .get("/api/paper/list")
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(1))
+                .body("id", hasItem(paperId.intValue()));
+    }
 }
 
 
